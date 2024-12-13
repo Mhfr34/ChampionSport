8fb5c3938bf490c9e76bad3c84cc2a48aea7910d
Mhamad  Farhat <mfarhat26@gmail.com>
2024-12-13 20:15:46 +0200

    trying to fix deploy

--- server/index.js
+++ server/index.js
@@ -8,33 +8,36 @@ const router = require("./routes");
 const app = express();
 
 // Middleware to parse JSON requests
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // CORS middleware function
 const allowCors = (fn) => async (req, res) => {
-    const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
 
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
-    );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, PATCH, DELETE, POST, PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, X-Auth-Token, X-User-Agent, X-Request-Id, X-Forwarded-For, X-Forwarded-Host, X-Forwarded-Proto, X-Forwarded-Port, X-Forwarded-Uri, X-Forwarded-Protocol"
+  );
 
-    // Handle preflight requests (OPTIONS)
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
+  // Handle preflight requests (OPTIONS)
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
 
-    // Call the actual request handler function
-    return await fn(req, res);
+  // Call the actual request handler function
+  return await fn(req, res);
 };
 
 // Apply CORS middleware to all routes under /api
@@ -43,8 +46,8 @@ app.use("/api", allowCors(router));
 // Connect to database
 const PORT = process.env.PORT || 8080;
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("Connected to DB");
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log("Connected to DB");
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
