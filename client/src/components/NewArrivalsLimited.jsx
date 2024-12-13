a193f93a646bee95e0bfad15c918096d0a423087
Mhamad  Farhat <mfarhat26@gmail.com>
2024-12-13 17:50:42 +0200

    trying to fix deploy

--- client/src/components/NewArrivalsLimited.jsx
+++ client/src/components/NewArrivalsLimited.jsx
@@ -124,7 +124,7 @@ const NewArrivalsLimited = () => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8080/api/last-eight-product"
+          "https://champion-sport.vercel.app/api/last-eight-product"
         );
         setProducts(response.data.data);
         setLoading(false);
