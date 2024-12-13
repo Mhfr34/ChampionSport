5a2efbf9383e9a6b9e642d1366fb64dc9d6b8284
Mhamad  Farhat <mfarhat26@gmail.com>
2024-12-13 19:35:38 +0200

    trying to fix deploy

--- server/index.js
+++ server/index.js
@@ -21,20 +21,21 @@ const allowCors = (fn) => async (req, res) => {
     }
 
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE, PUT');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     );
 
+    // Handle preflight requests (OPTIONS)
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
     }
 
+    // Call the actual request handler function
     return await fn(req, res);
 };
-
 
 // Apply CORS middleware to all routes under /api
 app.use("/api", allowCors(router));
