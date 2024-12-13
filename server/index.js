5e5b77c716e1cf73d5c1bfd3974405b353dc8f36
Mhamad  Farhat <mfarhat26@gmail.com>
2024-12-13 16:29:07 +0200

    trying to fix deploy

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const router = require("./routes");
@@ -12,31 +12,41 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
-// CORS middleware configuration
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Allow requests without an Origin header (like Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Allow credentials like cookies, headers
-  })
-);
+// CORS middleware function
+const allowCors = (fn) => async (req, res) => {
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Handle preflight requests (OPTIONS)
+  if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+  }
+
+  // Call the actual request handler function
+  return await fn(req, res);
+};
 
 // Route middleware
-app.use("/api", router);
+app.use("/api", allowCors(router));
+
 
 // Connect to database
 const PORT = process.env.PORT || 8080;
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Connected to DB");
-    console.log(`Server is running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log("Connected to DB");
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
